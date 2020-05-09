854df3f2cd39a1bbf0dac6663372418d5e842f42
Renjie Sun <sunrenjie6@gmail.com>
2020-05-09 09:05:31 +0800

    To support [optional] proxy for accessing google recaptcha service and the smtp mail server

--- src/main/java/com/baeldung/spring/CaptchaConfig.java
+++ src/main/java/com/baeldung/spring/CaptchaConfig.java
@@ -1,5 +1,6 @@
 package com.baeldung.spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,20 +9,30 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
 @Configuration
 @ComponentScan(basePackages = { "com.baeldung.captcha" })
 public class CaptchaConfig {
+    // Proxy configuration copied from https://stackoverflow.com/questions/3687670/
     @Bean
-    public ClientHttpRequestFactory clientHttpRequestFactory() {
+    public ClientHttpRequestFactory clientHttpRequestFactory(
+            @Value("${google.recaptcha.proxyHost:}") String proxyHost,
+            @Value("${google.recaptcha.proxyPort:0}") int proxyPort) {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        if (!proxyHost.equals("") && proxyPort > 0 && proxyPort <= 65535) {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+            factory.setProxy(proxy);
+        }
         factory.setConnectTimeout(3 * 1000);
         factory.setReadTimeout(7 * 1000);
         return factory;
     }
 
     @Bean
-    public RestOperations restTemplate() {
-        RestTemplate restTemplate = new RestTemplate(this.clientHttpRequestFactory());
+    public RestOperations restTemplate(ClientHttpRequestFactory clientHttpRequestFactory) {
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
         return restTemplate;
     }
 }
